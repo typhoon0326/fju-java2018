ac0511a0b8352fbfb65f0044677c250152a90199
簡佩璇 <user@es302n06>
2018-10-02 11:11:26 +0800

    exam

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -4,7 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello");
+        
+	System.out.println("today is so cold");
+	System.out.println("why?");
+	System.out.println("because i don't have a jacket");
+	System.out.println("remember to bring it tomorrow");
+	System.out.println("please...");
+	
 	}
-
 }
